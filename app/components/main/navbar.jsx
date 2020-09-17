81eba6b3e0f3eae9a777594847af8ecb9f1ba10b
Tirkirill <sherbakirill@mail.ru>
2020-09-17 10:21:53 +0300

    images

--- app/components/main/navbar.jsx
+++ app/components/main/navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import infoButton from "../../images/infoButton.png";
+import timeButton from "../../images/timeButton.png";
+import deleteButton from "../../images/deleteButton.png";
 
 const Navbar = (props)=> {
     return(
         <div className='navbar'>
             <div className='leftBar'></div>
             <div className='rightBar'>
-                <img className='infoButton' src=""></img>
+                <img className='infoButton' onClick={()=>console.log('info!')} src={infoButton}></img>
+                <img className='timeButton' onClick={()=>console.log('time!')} src={timeButton} ></img>
+                <img className='deleteButton' onClick={()=>console.log('delete!')} src={deleteButton} ></img>
                 <div className='email'>{props.user.email}</div>
             </div>
         </div>
