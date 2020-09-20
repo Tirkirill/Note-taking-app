dd0bb23690f397efc8a41bf94cbe90faf1c37134
Tirkirill <sherbakirill@mail.ru>
2020-09-20 19:29:55 +0300

    Редактор

--- app/components/main/main.jsx
+++ app/components/main/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./navbar.jsx";
 import Tagbar from "./tagbar.jsx";
 import Notes from "./notes.jsx";
-import noteReplace from "./js/noteReplace";
 
 let notes = [
     {
@@ -40,8 +39,7 @@ class Main extends React.Component {
 
     saveNote(title, text, currentNote) {
         let newNotes = this.state.notes.slice();
-        let newText = noteReplace(text);
-        newNotes[currentNote] = {title:title, text:newText, uniqueId: newNotes[currentNote]};
+        newNotes[currentNote] = {title:title, text:text, uniqueId: newNotes[currentNote]};
         this.setState({
             notes:newNotes
         })
