23e0ab8901d197c91e14cbf9f1a5ee12d160be4b
Tirkirill <sherbakirill@mail.ru>
2020-09-20 11:58:05 +0300

    Заменил рефами

--- app/components/main/tagbar.jsx
+++ app/components/main/tagbar.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { createRef } from "react";
 
-const Tagbar = (props)=> {
-    return (
-        <div className='grid bar tagbar'>
-            <div className='leftBar'>
-                <div className='tags'>Записи</div>
+class Tagbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.tagInput = React.createRef();
+    }
+
+    render() {      
+        return (
+            <div className='grid bar tagbar'>
+                <div className='leftBar'>
+                    <div className='tags'>Записи</div>
+                </div>
+                <div className='rightBar'>
+                    <input ref={this.tagInput} className='addTagInput' placeholder="Добавить запись..."></input>
+                    <button className='addTag' onClick={()=>{this.props.addNote(this.tagInput.current.value)}}></button>
+                </div>
             </div>
-            <div className='rightBar'>
-                <input className='addTagInput' placeholder="Добавить запись..."></input>
-                <button className='addTag' onClick={()=>{props.addNote(document.querySelector('.addTagInput').value)}}></button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Tagbar;
