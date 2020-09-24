2914531b11c267416cb6f7dd52d0c83d00021933
Tirkirill <sherbakirill@mail.ru>
2020-09-24 11:27:01 +0300

    Валидация пароля

--- app/components/forms/registration/myform.jsx
+++ app/components/forms/registration/myform.jsx
@@ -1,16 +1,41 @@
 import React from "React";
 
-const MyForm = (props)=> {
-    return(
-        <div className="formContainer">
-            <div><h1>{props.title}</h1></div>
-            <div className="form">
-                <input placeholder="Email"></input>
-                <input placeholder="Пароль"></input>
-                <button className="formButton" onClick={props.onClickHandler}></button>
+class MyForm extends React.Component{
+    constructor(props) {
+        super(props);
+        this.onClickHandler = this.onClickHandler.bind(this);
+        this.emailInput = React.createRef();
+        this.passwordInput = React.createRef();
+    }
+
+    onClickHandler() {
+        if (!this.emailInput.current.validity.valid) {
+            return
+        }
+        let email = this.emailInput.current.value;
+        let password = this.emailInput.current.value;
+        if (email.length > 5) {
+            this.props.onClickHandler();
+        }
+        else {
+            console.log("Ошибка");
+        }
+    }
+
+
+    render() {
+        return(
+            <div className="formContainer">
+                <div><h1>{this.props.title}</h1></div>
+                <div className="form">
+                    <input ref={this.emailInput} type="email" placeholder="Email"></input>
+                    <input pattern="[A-Za-z-0-9]{6,}" ref={this.passwordInput} placeholder="Пароль (>6 символов)"></input>
+                    <button className="formButton" onClick={this.onClickHandler}></button>
+                    <div>Войти</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default MyForm;
