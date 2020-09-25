6a03e526333d62689cdf37fa781d13af0a0349f2
Tirkirill <sherbakirill@mail.ru>
2020-09-25 22:09:28 +0300

    Доделал регистрацию и вход в аккаунт

--- app/components/forms/registration/login.jsx
+++ app/components/forms/registration/login.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import MyForm from "../myform.jsx";
-
+import firebase from "../../../backend/core";
+import { Redirect, Router } from "react-router-dom";
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded:false
+        }
+        this.login = this.login.bind(this);
+    }
+
+
+    login(email, password) {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(()=> {
+                this.setState(
+                    {
+                        loaded:true,
+                    }     
+                )
+            }
+        );
+    }
+
     render() {
         return (
-            <MyForm title="Вход" linkTo="/registration" formLinkText="Нет аккаунта?" onClickHandler={()=>console.log("Вход")}/>
+            <div>
+                {this.state.loaded && <Redirect to="/"/>}
+                <MyForm title="Вход" linkTo="/registration" formLinkText="Нет аккаунта?" onClickHandler={this.login}/>
+            </div>
         )
     }
 }
